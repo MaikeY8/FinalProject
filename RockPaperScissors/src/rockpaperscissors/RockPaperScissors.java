afc606c6fc6673c6e320a270c3822dce66c11f00
MaikeY8 <108020332+MaikeY8@users.noreply.github.com>
2022-06-24 10:15:09 -0500

    Add files via upload

---
+++ RockPaperScissors/src/rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,21 @@
+/** Required package class namespace */
+package rockpaperscissors;
+
+/**
+ * Main class that begins runs the main menu for rock paper scissors.
+ * 
+ * @author m.yan
+ * @since 9-June-18
+ */
+public class RockPaperScissors {
+    
+    /**
+     * The main code of the class
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MenuScreen screen = new MenuScreen();
+    }
+    
+}
